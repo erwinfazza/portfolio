8263087af51e742a8262f71466e1e6fea695370f
erwin <erwinintifazza@gmail.com>
2022-11-23 21:27:59 +0700

    menambahkan current year

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
+
+
+const year = document.querySelector('#current-year');
+
+year.innerHTML = new Date().getFullYear()
